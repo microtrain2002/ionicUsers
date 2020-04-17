3a6945a4dcaed8697b868f97fefd1b5a599b1144
chad svastisalee <swasgrafx@gmail.com>
2020-04-16 21:32:02 -0500

    Added ability to view Users and Single User

--- src/app/user.model.ts
+++ src/app/user.model.ts
@@ -1,7 +1,8 @@
-export class User {
+export class User{
+    _id: string;
     email: string;
-    username: string;
     first_name: string;
     last_name: string;
+    username: string;
     password: string;
-  }
+}
